ec2e2bf0cfa108357a31cb53bdf1d8924dfa43d1
Andrea Cabral <andrea.cabralb@gmail.com>
2016-05-27 10:26:00 -0700

    commented out websocket on client

--- app/assets/javascripts/medias.js
+++ app/assets/javascripts/medias.js
@@ -1,15 +1,15 @@
-var FFWebsocketManager = (function () {
-    var scheme = 'ws://';
-    var uri = scheme + 'localhost:3000/medias/stream';
-    var ws = new WebSocket(uri);
-    console.log('****** created a new websocket connection');
+// var FFWebsocketManager = (function () {
+//     var scheme = 'ws://';
+//     var uri = scheme + 'localhost:3000/medias/stream';
+//     var ws = new WebSocket(uri);
+//     console.log('****** created a new websocket connection');
 
-    ws.onopen = function (event) {
-        ws.send('Can you hear me?');
-    };
+//     ws.onopen = function (event) {
+//         ws.send('Can you hear me?');
+//     };
     
-    ws.onmessage = function (event) {
-        console.log('****** Received a message!');
-        console.log(event.data);
-    };
-})();
+//     ws.onmessage = function (event) {
+//         console.log('****** Received a message!');
+//         console.log(event.data);
+//     };
+// })();
