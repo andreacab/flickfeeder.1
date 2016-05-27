f05926a59f65bf380679bd20202719ab8b64d803
Andrea Cabral <andrea.cabralb@gmail.com>
2016-05-27 15:21:38 -0700

    test to verify if ws has a client would return false for index 0

--- lib/shrimp.rb
+++ lib/shrimp.rb
@@ -6,10 +6,11 @@ class Shrimp
 
     # Class methods
     def self.has_client(user_id)
-        !!@@clients.index do |client| 
+        puts '####### 999 ######'
+        @@clients.index do |client| 
             puts client.env["rack.session"]["warden.user.user.key"]
             client.env["rack.session"]["warden.user.user.key"][0][0] == user_id
-        end 
+        end > -1 
     end
 
     def self.clients
