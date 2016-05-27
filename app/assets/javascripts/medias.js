d30c8dbdb4fe4b7012441e9f7f8b113f84038dd0
Andrea Cabral <andrea.cabralb@gmail.com>
2016-05-27 13:01:20 -0700

    places ws JS into corresponding view

--- app/assets/javascripts/medias.js
+++ app/assets/javascripts/medias.js
@@ -1,31 +1,32 @@
-var FFWebsocketManager = (function () {
-    var scheme = 'ws://';
-    var uri = scheme + 'localhost:3000/medias/stream/';
-    var ws = new WebSocket(uri);
-    console.log('[FFWebsocketManager] created a new websocket connection');
+// var FFWebsocketManager = (function () {
+//     var scheme = 'ws://';
+//     var uri = scheme + 'localhost:3000/medias/stream/';
+//     var ws = new WebSocket(uri);
+//     console.log('[FFWebsocketManager] created a new websocket connection');
     
-    ws.onopen = function (event) {
-        ws.send('[FFWebsocketManager] Can you hear me?', event);
-        // console.log(getCookie('_flickfeeder_session'));
-    };
+//     ws.onopen = function (event) {
+//         console.log('[FFWebsocketManager] Opeing ws!!');
+//         ws.send('[FFWebsocketManager] Can you hear me?', event);
+//         // console.log(getCookie('_flickfeeder_session'));
+//     };
     
-    ws.onmessage = function (event) {
-        console.log('[FFWebsocketManager] Received a message!', event);
-        console.log(event.data);
-    };
+//     ws.onmessage = function (event) {
+//         console.log('[FFWebsocketManager] Received a message!', event);
+//         console.log(event.data);
+//     };
 
-    ws.onclose = function (event) {
-        console.log('[FFWebsocketManager] Closed...', event);
-    };
+//     ws.onclose = function (event) {
+//         console.log('[FFWebsocketManager] Closed...', event);
+//     };
 
-    ws.onerror = function (event) {
-        console.log('[FFWebsocketManager] Error....', event)
-    };
+//     ws.onerror = function (event) {
+//         console.log('[FFWebsocketManager] Error....', event)
+//     };
 
-    return {
-        send: function (message) {
-            console.log('Hello!');
-            ws.send("Hello!");
-        }
-    };
-})();
+//     return {
+//         send: function (message) {
+//             console.log('Hello!');
+//             ws.send("Hello!");
+//         }
+//     };
+// })();
