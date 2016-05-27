8a2b838cd7491b4a8c8e8c29a0bfa1ccf3ede856
Andrea Cabral <andrea.cabralb@gmail.com>
2016-05-27 11:21:35 -0700

    added check for cursor when retrieving medias

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -6,7 +6,6 @@ class MediasController < ApplicationController
     def index
         @dropbox_thumbnails = []
         if(hasDropboxAccount?(current_user))
-            puts '0**************************'
             @dropbox_thumbnails = get_dropbox_thumbnails
         end
 	end
@@ -14,27 +13,23 @@ class MediasController < ApplicationController
     private
 
     def get_dropbox_thumbnails
-        puts '1**************************'
         thumbs = []
         res = list_folder({ path: "", recursive: true, include_media_info: true }, current_user.dropbox_access_token)
-        puts '2**************************'
-        entries = JSON.parse(res.body)['entries']
-        puts '3**************************'
-        puts res
-        # current_user.dropbox_cursor = JSON.parse(res.body)['cursor']
-        # puts '4**************************'
-        # current_user.save!
-        puts '5**************************'
+        data = JSON.parse(res.body)
+        entries = data['entries']
+        cursor = data['cursor']
+        
+        # if cursor
+        #     current_user.dropbox_cursor = JSON.parse(res.body)['cursor']
+        #     current_user.save!
+        # end
 
         entries.each do |item|
-            puts '6**************************'
             if ( item['media_info'] && ( item['media_info']['metadata']['.tag'] == 'photo' ) )
-                puts '7**************************'
                 res = get_temporary_link({path: item['path_lower']}, current_user.dropbox_access_token)
                 thumbs.push(JSON.parse(res.body))
             end
         end
-        puts '8**************************'
 
         return thumbs
     end
