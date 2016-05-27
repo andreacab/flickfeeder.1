629126837e5ac92d141277afae4b7058b3a97a43
Andrea Cabral <andrea.cabralb@gmail.com>
2016-05-27 16:57:44 -0700

    added nil test for teammates array in corresponding view

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,6 +6,7 @@ class HomepageController < ApplicationController
 	def index
      @events = Event.where("end_date > ? AND organization_id = ? ", DateTime.now, current_user.organization_id )
      @teammates = User.where(organization_id: current_user.organization_id).where.not(id: current_user.id)
+     puts @teammates.inspect
      @statistics = []
 	end
 
