2efeed32a156a1ed7ee5392ecbdbd7d09e9ff353
Andrea Cabral <andrea.cabralb@gmail.com>
2016-05-30 18:41:20 -0700

    delete old Footer and replaced by es6 version

--- app/assets/javascripts/components/footer.es6.jsx
+++ app/assets/javascripts/components/footer.es6.jsx
@@ -1,5 +1,5 @@
 class Footer extends React.Component {
-    render() {
-        return <div id="footer"><p id="p">{'I am a footer'}</p></div>;
-    }
+  render () {
+    return <div id="footer"><p id="p">{'I am a footer'}</p></div>;
+  }
 }
