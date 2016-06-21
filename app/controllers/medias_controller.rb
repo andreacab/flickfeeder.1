63b600be6e73bebae9d61866c87736ef1e4aa18c
Andrea Cabral <andrea.cabralb@gmail.com>
2016-06-20 21:12:13 -0700

    fetch thumbnails images

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -13,15 +13,30 @@ class MediasController < ApplicationController
 
     private
 
-    def get_dropbox_thumbnails
+    def get_user_dropbox_entries
         res = list_folder({ path: "", recursive: true, include_media_info: true }, current_user.dropbox_access_token)
         data = JSON.parse(res.body)
-        entries = data['entries']
-        cursor = data['cursor']
-
+        
+        cursor = data['cursor'] 
         current_user.update_attributes( dropbox_cursor: cursor ) if cursor
 
-        get_temporary_links(entries, current_user.dropbox_access_token)
+        data['entries']
     end
 
+    def get_dropbox_thumbnails  
+        # get all files info from current user
+        entries = get_user_dropbox_entries
+        binding.pry
+
+        # get thumbnails' base-64 encoded string 
+        get_thumbnails(entries, current_user.dropbox_access_token).reject { |base64String| !base64String }
+    end
+
+    def get_dropbox_temp_links
+        # get all files info from current user
+        entries = get_user_dropbox_entries
+
+        # get temporary links of full resolution images
+        get_temporary_links(entries, current_user.dropbox_access_token).reject { |url| !url }
+    end
 end
--- app/helpers/users/dropbox_helper.rb
+++ app/helpers/users/dropbox_helper.rb
@@ -1,8 +1,23 @@
 require 'net/http'
 
 module Users::DropboxHelper
+    THUMB_MAX_SIZE = 20_000_000 # in bytes
+
+    THUMB_SIZES = {
+        w32h32: 'w32h32',
+        w64h64: 'w64h64',
+        w128h128: 'w128h128',
+        w640h480: 'w640h480',
+        w1024h768: 'w1024h768'
+    }
+
+    FORMATS = {
+        jpg: 'jpeg',
+        png: 'png'
+    }
+
     def get_temporary_link(data, access_token)
-        res = post_req(
+        post_req(
             'https://api.dropboxapi.com/2/files/get_temporary_link',
             data, 
             {
@@ -13,7 +28,7 @@ module Users::DropboxHelper
     end
 
     def list_folder(data, access_token)
-        res = post_req(
+        post_req(
             'https://api.dropboxapi.com/2/files/list_folder',
             data,
             {
@@ -23,15 +38,29 @@ module Users::DropboxHelper
         )
     end
 
-    def list_folder_continue(data, access_token)
-        res = post_req(
+    def self.test
+        binding.pry
+    end
+
+    def self.list_folder_continue(data, access_token)
+        post_req(
             'https://api.dropboxapi.com/2/files/list_folder/continue',
             data,
             {
                 'Authorization' => 'Bearer ' + access_token, 
                 'Content-type' => 'application/json'
             }
-        )
+        )   
+    end
+
+    def get_thumbnail(data, access_token)
+        get_req(
+            'https://content.dropboxapi.com/2/files/get_thumbnail',
+            {
+                'Authorization' => 'Bearer ' + access_token, 
+                'Dropbox-API-Arg' => data.to_json
+            }
+        ) 
     end
 
     # entries is data received when accessing dropbox API endpoints list_folder or list_folder_continue 
@@ -43,7 +72,24 @@ module Users::DropboxHelper
             else
                 nil
             end
-        end.reject { |path| !path }
+        end
+    end
+
+    def get_thumbnails(entries, access_token)
+        entries.map do |entry|
+            return nil if entry['size'] >= THUMB_MAX_SIZE
+
+            if (entry['path_lower'] && entry['media_info'] && entry['media_info']['metadata']['.tag'] == 'photo')
+                data = get_thumbnail({path: entry['id'], format: FORMATS[:jpg], size: THUMB_SIZES[:w640h480]}, access_token)
+                if data.body
+                    'data:image/' + FORMATS[:jpg] + ';base64,' + Base64.strict_encode64(data.body)
+                else 
+                    nil
+                end
+            else
+                nil
+            end
+        end
     end
 
     def hasDropboxAccount?(user)
@@ -69,7 +115,17 @@ module Users::DropboxHelper
         headers.each { |name, value| req[name] = value }
 
         res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-          http.request(req)
+            http.request(req)
+        end
+    end
+
+    def get_req(address, headers)
+        uri = URI(address)
+        req = Net::HTTP::Get.new(uri)
+        headers.each { |name, value| req[name] = value }
+        
+        res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+            http.request(req)
         end
     end
 end
